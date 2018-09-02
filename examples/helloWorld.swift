597ef3d62a66aebf93a5276f92b0b63f21615792
David Walter <hello@davidwalter.at>
2018-09-02 22:11:05 +0200

    Adds Swift example

---
+++ examples/helloWorld.swift
@@ -0,0 +1,18 @@
+//
+// helloWorld.swift
+// HelloWorld
+//
+
+class HelloWorld {
+
+    init() {
+
+    }
+
+    func write(message: String) {
+        print(message)
+    }
+}
+
+let hello = HelloWorld()
+hello.write(message: "Hello World!")
